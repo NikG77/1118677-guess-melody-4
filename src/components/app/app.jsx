19f3a5686ff4d3afed35cac97607e9bf1c260328
Georg <georgiy.nikitin@gmail.com>
2020-06-14 10:02:21 +0300

    Добавит обработчик клика для кнопки и обновит снепшот тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import WelcomScreen from "../welcome-screen/welcome-screen.jsx";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from "prop-types";
+
+const welcomeButtonHandler = () => {};
 
 const App = (props) => {
 
   const {errorsCount} = props;
   return (
-    <WelcomScreen errorsCount={errorsCount}/>
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
 
