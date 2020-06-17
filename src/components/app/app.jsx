6e779f8a33e2e4b12ff6f497ca16d84248f07c02
Georg <georgiy.nikitin@gmail.com>
2020-06-17 13:29:19 +0300

    Добавит в app router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,44 @@
 import React, {PureComponent} from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import {GameType} from "../../const.js";
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      step: -1,
+    };
   }
 
   render() {
-    const {errorsCount} = this.props;
+    const {questions} = this.props;
 
-    return <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={() => {}}
-    />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
