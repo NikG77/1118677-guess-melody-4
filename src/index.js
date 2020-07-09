a731e3fc28518927a4101f470a1675448e8ac42a
Georg <georgiy.nikitin@gmail.com>
2020-07-09 18:28:50 +0300

    Вернет поддержку Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
-import {reducer} from "./reducer/reducer.js";
+import reducer from "./reducer/reducer.js";
 import thunk from "redux-thunk";
 import {Operation as DataOperation} from "./reducer/data/data.js";
 import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
 import {createAPI} from "./api.js";
+import {composeWithDevTools} from "redux-devtools-extension";
+
 
 const onUnauthorized = () => {
   store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
@@ -17,15 +19,16 @@ const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-    applyMiddleware(thunk.withExtraArgument(api))
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 store.dispatch(DataOperation.loadQuestions());
 store.dispatch(UserOperation.checkAuth());
 
 
-ReactDom.render(
+ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>,
